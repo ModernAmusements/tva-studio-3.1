4a59c7569233d9cefb55cc1c34fe33379d901697
ModernAmusements <st@zahnarzt-helden.de>
2021-04-02 20:30:20 +0200

    header cats

--- resources/themes/tva/views/layouts/header/nav-menu/navmenu.blade.php
+++ resources/themes/tva/views/layouts/header/nav-menu/navmenu.blade.php
@@ -1,20 +1,18 @@
-
-    {{-- Hidden Cats --}}
-    <?php
-        $categories = [];
-        foreach (app('Webkul\Category\Repositories\CategoryRepository')->getVisibleCategoryTree(core()->getCurrentChannel()->root_category_id) as $category){
-            if ($category->slug)
-                array_push($categories, $category);
-        }
-    ?>
-    @if (count($categories))
-        <div class="categories-toggle">
-            <ul class="nav">
-                @foreach ($categories as $key => $category)
-                    <li class="medium" class="{{ Request::is($category->slug) ? ' active' : '' }}" >
-                        <a class="left" href="{{ route('shop.productOrCategory.index', $category->slug) }}">{{ $category->name }}</a>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+<?php
+    $categories = [];
+    foreach (app('Webkul\Category\Repositories\CategoryRepository')->getVisibleCategoryTree(core()->getCurrentChannel()->root_category_id) as $category){
+        if ($category->slug)
+            array_push($categories, $category);
+    }
+?>
+@if (count($categories))
+    <div class="categories-toggle">
+        <ul class="nav">
+            @foreach ($categories as $key => $category)
+                <li class="medium">
+                    <a class="left {{ Request::is($category->slug) ? ' active' : '' }}"  href="{{ route('shop.productOrCategory.index', $category->slug) }}">{{ $category->name }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+@endif
