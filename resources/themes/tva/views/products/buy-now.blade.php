66cde8bc14f6a4d0a1e532d0a239be0be832fc8d
ModernAmusements <st@zahnarzt-helden.de>
2021-04-10 10:50:44 +0200

    buy now bug

--- resources/themes/tva/views/products/buy-now.blade.php
+++ resources/themes/tva/views/products/buy-now.blade.php
@@ -1,8 +1,10 @@
 {!! view_render_event('bagisto.shop.products.buy_now.before', ['product' => $product]) !!}
 <div class="article-cart article-cart-subpage-now btn-grid btn-grid-black">
+
 <button type="submit" class="btn-hover buynow" {{ ! $product->isSaleable(1) ? 'disabled' : '' }}>
     {{ __('shop::app.products.buy-now') }}
 </button>
+
 </div>
 
 {!! view_render_event('bagisto.shop.products.buy_now.after', ['product' => $product]) !!}
