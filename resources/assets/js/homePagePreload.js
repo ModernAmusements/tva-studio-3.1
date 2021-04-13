f64cd46a54f8936185faf9e0173a9399d78cd74a
ModernAmusements <st@zahnarzt-helden.de>
2021-04-13 12:11:56 +0200

    preloader fertig

--- resources/assets/js/homePagePreload.js
+++ resources/assets/js/homePagePreload.js
@@ -6,6 +6,7 @@ $(function() {
   if (!sessionStorage.getItem('homePagePreloader')) {
     sessionStorage.setItem('homePagePreloader', true);
     $('#preload-homepage').addClass('loaded').scrollTop(0);
+    $('body').addClass('dark-background');
      setTimeout(function() {
         $('#preload-homepage').addClass('fade-in');
         }, 2500);
@@ -16,6 +17,7 @@ $(function() {
         }, 3500);
 
     setTimeout(function() {
+        $('body').removeClass('dark-background');
         $('header').css('display', 'grid');
         $('#app').css('display', 'block');
         $('#header-bottom').css('display', 'grid');
@@ -23,25 +25,21 @@ $(function() {
         }, 3500);
 
     setTimeout(function() {
-        $('#app').addClass('fade-up');
-        $('header').addClass('fade-up');
-        $('#header-bottom').addClass('fade-up');
+        $('#app').add('header').add('#header-bottom').addClass('fade-up');
         }, 3525);
 
   }
   else {
+
     $('#preload-homepage').css('display', 'none');
     setTimeout(function() {
-        $('header').addClass('nav-header-animation');
-        $('header').css('display', 'grid');
+        $('header').addClass('nav-header-animation').css('display', 'grid');
         $('#app').css('display', 'block');
         $('#header-bottom').css('display', 'grid');
         }, 10);
     setTimeout(function() {
         $('body').removeClass('animation');
-        $('#app').addClass('fade-up');
-        $('header').addClass('fade-up');
-        $('#header-bottom').addClass('fade-up');
+        $('#app').add('header').add('#header-bottom').addClass('fade-up');
         }, 100);
   }
 });
